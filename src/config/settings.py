3346be22f5a894646fc38cafc5a097551fc0b999
aovsybo <matgr2n@gmail.com>
2023-10-01 21:00:14 +0300

    added CORS_ALLOWED_ORIGINS

--- src/config/settings.py
+++ src/config/settings.py
@@ -185,3 +185,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://84.38.182.17:5173',
+]
