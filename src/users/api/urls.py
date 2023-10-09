4c96338c3bad40d4a0560663af5399089d2ee160
aovsybo <matgr2n@gmail.com>
2023-10-09 22:05:43 +0300

    changed account urls

--- src/users/api/urls.py
+++ src/users/api/urls.py
@@ -4,8 +4,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.api import views
 
 urlpatterns = [
-    path('register/', views.RegisterAPI.as_view()),
-    path('verify/', views.VerifyAPI.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('account/register/', views.RegisterAPI.as_view()),
+    path('account/verify/', views.VerifyAPI.as_view()),
+    path('account/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('account/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
